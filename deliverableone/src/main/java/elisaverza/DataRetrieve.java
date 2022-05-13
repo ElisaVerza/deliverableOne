c69880af0aa5b5b9239b2b3ef330b84128368156
ElisaVerza <velis.smile@gmail.com>
2022-05-13 16:23:29 +0200

    sha added to commit data, sonar-like extension

--- deliverableone/src/main/java/elisaverza/DataRetrieve.java
+++ deliverableone/src/main/java/elisaverza/DataRetrieve.java
@@ -23,6 +23,7 @@ public class DataRetrieve
     private static final String CSV_JIRA = "jiradata.csv";
     private static final String PRJ_NAME = "SYNCOPE";
     private static final String USERNAME = "ElisaVerza";
+    private static final boolean DOWNLOAD_COMMIT = true;
     private static final String AUTH_CODE = "/home/ella/vsWorkspace/auth_code.txt";
 
     public static InputStreamReader auth(URL url) throws IOException{
@@ -42,7 +43,7 @@ public class DataRetrieve
         return new InputStreamReader(uc.getInputStream());
     }
 
-    private static String read_all(BufferedReader rd) throws IOException {
+    private static String readAll(BufferedReader rd) throws IOException {
         StringBuilder sb = new StringBuilder();
         int cp;
         while ((cp = rd.read()) != -1) {
@@ -51,7 +52,7 @@ public class DataRetrieve
         return sb.toString();
      }
 
-     public static JSONArray read_json_array_from_url(String url, Boolean git) throws IOException, JSONException {
+     public static JSONArray readJsonArrayFromUrl(String url, boolean git) throws IOException, JSONException {
         InputStreamReader is;
         if(git){
             URL strToUrl = new URL(url);
@@ -61,18 +62,16 @@ public class DataRetrieve
             InputStream iStream = new URL(url).openStream();
             is = new InputStreamReader(iStream);
         }
-        try {
-           BufferedReader rd = new BufferedReader(is);
-           String jsonText = read_all(rd);
+        try(BufferedReader rd = new BufferedReader(is)) {
+           String jsonText = readAll(rd);
 
-           JSONArray json = new JSONArray(jsonText);
-           return json;
+           return new JSONArray(jsonText);
          } finally {
            is.close();
          }
      }
   
-     public static JSONObject read_json_obj_from_url(String url, Boolean git) throws IOException, JSONException {
+     public static JSONObject readJsonObjFromUrl(String url, boolean git) throws IOException, JSONException {
         InputStreamReader is;
         if(git){
             URL strToUrl = new URL(url);
@@ -82,33 +81,18 @@ public class DataRetrieve
             InputStream iStream = new URL(url).openStream();
             is = new InputStreamReader(iStream);
         }
-        try {
-           BufferedReader rd = new BufferedReader(is);
-           String jsonText = read_all(rd);
-
-           JSONObject json = new JSONObject(jsonText);
-           return json;
+        try(BufferedReader rd = new BufferedReader(is)){
+           String jsonText = readAll(rd);
+           return new JSONObject(jsonText);
          } finally {
            is.close();
          }
      }
 
-    public static String parse_id(String toParse) throws SecurityException, IOException{
-        String parsed = "";
-        //Integer i = toParse.indexOf("#", 0);
+    public static String parseId(String toParse) throws SecurityException{
+        StringBuilder parsed = new StringBuilder();        
         Integer j = 0;
         Integer i = 0;
-
-        /*if(toParse.contains("BOOKKEEPER-")){
-            j = toParse.indexOf("BOOKKEEPER-");
-            while(toParse.length()>j){
-                if(Character.isDigit(toParse.charAt(j))){
-                    parsed = parsed+toParse.charAt(j);
-                    j++;
-                }
-                else{ break;}
-            }
-        }*/
 
         if(toParse.contains(PRJ_NAME)){
             j = toParse.indexOf(PRJ_NAME);
@@ -117,123 +101,121 @@ public class DataRetrieve
                 if(toParse.length()>j+8 && Character.isDigit(toParse.charAt(j+8))){
                     i = j+8;
                 }
-                parsed = parsed+toParse.charAt(j);
+                parsed.append(toParse.charAt(j));
                 j++;
             }
         }
-        return parsed;
-
-	    /*while(i>=0 && toParse.length()>i+1){
-	        if(Character.isDigit(toParse.charAt(i+1))){
-                System.out.println(toParse.length()+" "+i);
-	            while(toParse.length()>i+1 && Character.isDigit(toParse.charAt(i+1))){
-		            parsed = parsed+toParse.charAt(i+1);
-		            i++;
-	            }
-	            return parsed;
-	        }
-	        else{
-	            i = toParse.indexOf("#", i+1);}
-	    }
-        return parsed;*/
+        return parsed.toString();
     }
 
-    public static String searchCsvLine(int searchColumnIndex, String searchString) throws IOException {
-        String resultRow = null;
-        BufferedReader br = new BufferedReader(new FileReader(CSV_COMMIT));
-        String line;
-        while ( (line = br.readLine()) != null ) {
-            String[] values = line.split(",");
-            if(values[searchColumnIndex].equals(searchString)) {
-                resultRow = line;
-                break;
+    public static String[] searchCsvLine(int searchColumnIndex, String searchString) throws IOException {
+        String[] resultRow = new String[10];
+        Integer i = 0;
+        try(BufferedReader br = new BufferedReader(new FileReader(CSV_COMMIT))){
+            String line;
+            while ( (line = br.readLine()) != null ) {
+                String[] values = line.split(",");
+                if(values.length > 1 && values[searchColumnIndex].equals(searchString)) {
+                    resultRow[i] = line;
+                    i++;
+                }
             }
         }
-        br.close();
 
         return resultRow;
     }
 
-    public static void jira_data(FileWriter commitWriter) throws JSONException, IOException{
-        Integer j = 0, i = 0, total = 1;
+    public static void cvsPopulation(Integer i, JSONArray json, FileWriter commitWriter) throws IOException{
+        String jsonKey = "fields";
+
+        JSONArray versionArray = json.getJSONObject(i%1000).getJSONObject(jsonKey).getJSONArray("versions");
+        JSONArray fixVersionArray = json.getJSONObject(i%1000).getJSONObject(jsonKey).getJSONArray("fixVersions");
+        String[] version = new String[versionArray.length()];
+        String[] fixVersion = new String[fixVersionArray.length()];
+        Integer k = 0;
+        while(versionArray.length()!=0 && k<versionArray.length()){    
+            version[k] = versionArray.getJSONObject(k).getString("name");
+            k++;
+        }
+        k=0;
+        while(fixVersionArray.length()!=0 && k<fixVersionArray.length()){    
+            fixVersion[k] = fixVersionArray.getJSONObject(k).getString("name");
+            k++;
+        }
+
+        String key = json.getJSONObject(i%1000).get("key").toString();
+        String resDate = json.getJSONObject(i%1000).getJSONObject(jsonKey).get("resolutiondate").toString();
+        String created = json.getJSONObject(i%1000).getJSONObject(jsonKey).get("created").toString();
+        String[] commit = searchCsvLine(1, key);
+        String versionStr = Arrays.toString(version);
+        versionStr = versionStr.replace(",", " ");
+        String fixVersionStr = Arrays.toString(fixVersion);
+        fixVersionStr = fixVersionStr.replace(",", " ");
+        for(k=0;k<commit.length;k++){
+            if(commit[k] != null){
+                commit[k] = commit[k].replace("\n", " ");
+                commitWriter.append(commit[k]+","+resDate+","+versionStr+","+fixVersionStr+","+created+"\n");
+            }
+        }
+    }
+
+
+    public static void jiraData(FileWriter commitWriter) throws JSONException, IOException{
+        Integer j = 0;
+        Integer i = 0;
+        Integer total = 1;
         do {
             j = i + 1000;
-            JSONArray json = new JSONArray();
             String url = "https://issues.apache.org/jira/rest/api/2/search?jql=project=%22"
                     + PRJ_NAME + "%22AND%22issueType%22=%22Bug%22AND(%22status%22=%22closed%22OR"
-                    + "%22status%22=%22resolved%22)AND%22resolution%22=%22fixed%22&fields=key,resolutiondate,versions,created&startAt="
+                    + "%22status%22=%22resolved%22)AND%22resolution%22=%22fixed%22&fields=key,resolutiondate,versions,fixVersions,created&startAt="
                     + i.toString() + "&maxResults=" + j.toString();
-            JSONObject jsonObj = read_json_obj_from_url(url, false);
-            json = new JSONArray(jsonObj.getJSONArray("issues"));
+            JSONObject jsonObj = readJsonObjFromUrl(url, false);
+            JSONArray json = new JSONArray(jsonObj.getJSONArray("issues"));
             total = jsonObj.getInt("total");
 
             for (; i < total && i < j; i++) {
-                JSONArray versionArray = json.getJSONObject(i%1000).getJSONObject("fields").getJSONArray("versions");
-                String[] version = new String[versionArray.length()];
-                Integer k = 0;
-                while(versionArray.length()!=0 && k<versionArray.length()){    
-                    version[k] = versionArray.getJSONObject(k).getString("name").toString();
-                    k++;
-                }
-
-                String key = json.getJSONObject(i%1000).get("key").toString();
-                String resDate = json.getJSONObject(i%1000).getJSONObject("fields").get("resolutiondate").toString();
-                String created = json.getJSONObject(i%1000).getJSONObject("fields").get("created").toString();
-                String commit = searchCsvLine(1, key);
-                if(commit != null){
-                    commit = commit.replaceAll("\n", " ");
-                    commitWriter.append(commit+","+key+","+resDate+","+Arrays.toString(version)+","+created+"\n");
-                }
+                cvsPopulation(i, json, commitWriter);
             }  
       } while (i < total);
     }
 
 
-    public static void commit_data(FileWriter commitWriter) throws IOException, InterruptedException{
+    public static void commitData(FileWriter commitWriter) throws IOException, InterruptedException{
         Integer i = 1; 
         Integer l = 0;
         Integer k;
         String date;
+        String sha;
         String jiraId;
-        JSONArray jPage;
-        String temp;
         do{
-            jPage = new JSONArray();
-
             String url = "https://api.github.com/repos/apache/"+PRJ_NAME+"/commits?page="+i.toString()+"&per_page=100";
-            jPage = new JSONArray(read_json_array_from_url(url, true));
+            JSONArray jPage = new JSONArray(readJsonArrayFromUrl(url, true));
             l = jPage.length();
             Thread.sleep(1000);
             for(k=0; k<l; k++){
-                temp = jPage.getJSONObject(k).getJSONObject("commit").getString("message");
                 date = jPage.getJSONObject(k).getJSONObject("commit").getJSONObject("committer").getString("date");
-                jiraId = parse_id(jPage.getJSONObject(k).getJSONObject("commit").getString("message"));
-                temp = temp.replaceAll(",", " ");
-                temp = temp.replaceAll("\n", " ");
-                temp = temp.replaceAll("\r", " ");
-                temp = temp.replaceAll("\t", " ");
-                commitWriter.append(date + "," + jiraId +",");
-                commitWriter.append(temp);
-                commitWriter.append("\n");
-            }
-
-                
+                jiraId = parseId(jPage.getJSONObject(k).getJSONObject("commit").getString("message"));
+                sha = jPage.getJSONObject(k).getString("sha");
+                commitWriter.append(date + "," +sha+","+ jiraId +"\n");
+            }                
             i++;
         } while(l != 0);
     }
 
     public static void main( String[] args ) throws IOException, InterruptedException{
 
-        File commitFile = new File(CSV_COMMIT);
-        FileWriter commitWriter = new FileWriter(commitFile);
-        commitWriter.append("date,jira_id,comment\n");
-        commit_data(commitWriter);
-        commitWriter.close();
-
+        if(DOWNLOAD_COMMIT){
+            File commitFile = new File(CSV_COMMIT);
+            try(FileWriter commitWriter = new FileWriter(commitFile)){
+                commitWriter.append("date,jira_id,comment\n");
+                commitData(commitWriter);
+            }
+        }
         File jiraFile = new File(CSV_JIRA);
-        FileWriter jiraWriter = new FileWriter(jiraFile);
-        jira_data(jiraWriter);
-        jiraWriter.close();
-
+        try(FileWriter jiraWriter = new FileWriter(jiraFile)){
+            jiraWriter.append("jira_id,affected versions,fixed version,commit date,created,resolution date\n");
+            jiraData(jiraWriter);    
+        }
     }
 }
